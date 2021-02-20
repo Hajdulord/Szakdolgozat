8897ab3de3fd3be817fa09097f5b60e4a7486944
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2021-02-20 20:36:12 +0100

    Added Script InputController and comments

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 namespace HMF.Thesis.Player
 {
+    /// This class is used for storing the player's data.
     public class Player : MonoBehaviour
     {
-        [SerializeField] private int _health = 5;
-        [SerializeField] private int _speed = 10;
-        [SerializeField] private int _jumpForce = 5;
+        [SerializeField] private int _health = 5; ///< The player's health.
+        [SerializeField] private int _speed = 10; ///< The player's speed.
+        [SerializeField] private int _jumpForce = 5; ///< The player's jump force.
 
+        /// Public property for Health.
         public int Health
         {
             get{ return Mathf.Max(0, _health); }
             private set{ _health = Mathf.Max(0, value); }
         }
         
+        /// Public property for Speed.
         public int Speed
         {
             get{ return Mathf.Max(0, _speed); }
             private set{ _speed = Mathf.Max(0, value); }
         }
 
+        /// Public property for JumpForce.
         public int JumpForce
         {
             get{ return Mathf.Max(0, _jumpForce); }
