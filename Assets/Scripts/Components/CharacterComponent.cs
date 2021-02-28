cbcbff48951993a03a0b0f001287add1f2ca8013
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2021-02-28 13:37:44 +0100

    Finished CgaracterComponent.

--- Assets/Scripts/Components/CharacterComponent.cs
+++ Assets/Scripts/Components/CharacterComponent.cs
@@ -1,21 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using HMF.Thesis.Logic;
+using HMF.Thesis.Interfaces;
+using HMF.Thesis.ScriptableObjects;
 
 namespace HMF.Thesis.Components
 {
+    /// Character Component that wraps the CharacterLogic class.
+    [RequireComponent(typeof(SpriteRenderer))]
     public class CharacterComponent : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        [Header("Serialized Fields")]
+        [SerializeField] private CharacterData _characterData = null!; ///< The data of the Character.
+        private ICharacter _character; ///< The Logic that this class wraps Character.
 
-        // Update is called once per frame
-        void Update()
+        /// Getter for the Character logic.
+        public ICharacter Character => _character;
+
+        /// Sets basic values.        
+        private void Awake() 
         {
-        
+            _character = new CharacterLogic(_characterData);
+
+            GetComponent<SpriteRenderer>().sprite = _character.CharacterSprite;
         }
     }
 }
