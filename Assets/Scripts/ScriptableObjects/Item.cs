2ac4ad222f82dadb8defa8a01358e1761f18dec6
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2021-03-13 13:38:22 +0100

    Created Iem Scriptable Object-

---
+++ Assets/Scripts/ScriptableObjects/Item.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HMF.Thesis.ScriptableObjects
+{
+    public class Item : ScriptableObject
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
