7092642022e59aa02a42f01fd57a003ba58ec2a1
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2021-03-14 22:12:35 +0100

    Created Healable addon.

---
+++ Assets/Scripts/Components/HealableComponent.cs
@@ -0,0 +1,19 @@
+using HMF.Thesis.Interfaces;
+using HMF.Thesis.Interfaces.ComponentInterfaces;
+using HMF.Thesis.Logic;
+using UnityEngine;
+
+namespace HMF.Thesis.Components
+{
+    public class HealableComponent : MonoBehaviour, IHealableComponent
+    {
+        private IHealable _helable;
+        public IHealable Healable => _helable;
+
+        private void Awake() 
+        {
+            var character = GetComponent<ICharacter>();
+            _helable = new HealableLogic(character);
+        }
+    }
+}
