ecb38b1b1a5d4ddc8e2f0155fe6f297c331b9d06
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2021-03-15 15:23:48 +0100

    HealableLogic Test

---
+++ Assets/Scripts/Tests/HealableLogicTest.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using HMF.Thesis.Interfaces;
+using HMF.Thesis.Logic;
+using Moq;
+
+namespace HMF.Thesis.Tests
+{
+    public class HealableLogicTest
+    {
+        [Test]
+        public void HealWithDefaultValue()
+        {
+            //* Setup
+            var characterMoq = new Mock<ICharacter>();
+            characterMoq.SetupProperty(e => e.Health, 5);
+
+            var healable = new HealableLogic(characterMoq.Object);
+
+            //* Affect
+            healable.Heal();
+
+            //* Testing
+            Assert.AreEqual(6, characterMoq.Object.Health);
+        }
+
+        [Test]
+        public void HealWithSetValue()
+        {
+            //* Setup
+            var characterMoq = new Mock<ICharacter>();
+            characterMoq.SetupProperty(e => e.Health, 5);
+
+            var healable = new HealableLogic(characterMoq.Object);
+
+            //* Affect
+            healable.Heal(2);
+
+            //* Testing
+            Assert.AreEqual(7, characterMoq.Object.Health);
+        }
+    }
+}
