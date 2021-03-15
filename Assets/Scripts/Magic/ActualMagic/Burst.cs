225e1d613bc69f72e62f671665028f1e4cb851b1
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2021-03-15 22:55:07 +0100

    Finished Burst Magic.

---
+++ Assets/Scripts/Magic/ActualMagic/Burst.cs
@@ -0,0 +1,39 @@
+using HMF.Thesis.ScriptableObjects;
+using HMF.Thesis.Interfaces.ComponentInterfaces;
+using UnityEngine;
+
+namespace HMF.Thesis.Magic.ActualMagic
+{
+    public class Burst : MagicBase
+    {
+        
+
+        public override string Name => "Burst";
+
+        public override void Use(string[] tagsToIgnore, MagicFocusData magicFocus, Vector2 center, int dir = 0)
+        {
+            var colliders = Physics2D.OverlapCircleAll(center, magicFocus.attackRange);
+
+            foreach (var item in colliders)
+            {
+                var ignore = false;
+                foreach(var tag in tagsToIgnore)
+                {
+                    if (item.tag == tag)
+                    {
+                        ignore = true;
+                    }
+                }
+
+                if (!ignore)
+                {
+                    var damageable = item.gameObject.GetComponent<IDamageableComponent>();
+                    var statusHandler = item.gameObject.GetComponent<IStatusHandlerComponent>();
+
+                    damageable.Damageable.TakeDamage(magicFocus.damage);
+                    statusHandler.StatusHandler.AddStatus(magicFocus.status);
+                }
+            }
+        }
+    }
+}
