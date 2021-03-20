b01715ab5b0f5453bc6b3f06e4477647f0707ca3
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2021-03-20 14:24:44 +0100

    IAttack interface.

--- Assets/Scripts/Interfaces/IAttack.cs
+++ Assets/Scripts/Interfaces/IAttack.cs
@@ -1,12 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 //! Needs Implementation!
 namespace HMF.Thesis.Interfaces
 {
     public interface IAttack
     {
-        
+        void Attack(IItem item, string[] tagsToTarget);
     }
 }
