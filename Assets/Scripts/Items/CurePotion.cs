db2145355544911075ca63a57308d9ea6d610580
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2021-04-25 18:24:51 +0200

    Added new Item.

--- Assets/Scripts/Items/CurePotion.cs
+++ Assets/Scripts/Items/CurePotion.cs
@@ -1,41 +1,35 @@
 using UnityEngine;
-using HMF.Thesis.Interfaces.ComponentInterfaces;
 using HMF.Thesis.Interfaces;
 using HMF.Thesis.ScriptableObjects;
+using HMF.Thesis.Interfaces.ComponentInterfaces;
 
 namespace HMF.Thesis.Items
 {
-
-    public class HealthPotion : Consumable
+    public class CurePotion : Consumable
     {
         private ConsumableData _consumableData;
 
         public override string Name => _consumableData.consumableName;
-
         public override bool Unique => _consumableData.isUnique;
-
         public override Sprite Sprite => _consumableData.sprite;
-
         public override TargetType TargetType => _consumableData.targetType;
-
         public override string Description => _consumableData.description;
-
         public override float attackTime => _consumableData.attackTime;
 
-        public HealthPotion(ConsumableData consumable)
+        public CurePotion(ConsumableData consumable)
         {
             _consumableData = consumable;
         }
 
+
         public override void Use(GameObject origin, string[] tagsToTarget, LayerMask layersToTarget)
         {
-            var healable = origin.GetComponent<IHealableComponent>()?.Healable;
-
-            healable.Heal(_consumableData.affectAmount);
-
             var status = origin.GetComponent<IStatusHandlerComponent>()?.StatusHandler;
 
-            status.AddStatus(_consumableData.status.ToString());
+            if (status != null)
+            {
+                status.RemoveAllStatuses();
+            }
         }
     }
 }
